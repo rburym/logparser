5e5ad3d463b261f8df51d21f5fd37e052a2a1f51
rBurym <r.burym@gmail.com>
2025-03-20 13:28:13 +0300

    feat: README.MD, add full_errors in logparser.py

--- logparser.py
+++ logparser.py
@@ -10,27 +10,46 @@ def parse_logs(input_file, output_file):
     warn_messages = defaultdict(int)
     error_pattern = re.compile(r'ERROR.*?- (.*)')
     warn_pattern = re.compile(r'WARN.*?- (.*)')
+    full_error = re.compile(r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) (ERROR|WARN) .*? - (.*)')
+    full_errors = []
 
-    with open(input_file, 'r') as file:
+    with open(input_file, 'r', encoding='utf-8') as file:
         for line in file:
             if 'ERROR' in line:
                 errors += 1
                 match = error_pattern.search(line)
                 if match:
-                    message = match.group(1).strip()  # Извлекаем сообщение об ошибке
+                    message = match.group(1).strip()
                     error_messages[message] += 1
-                    output_lines.append(message)  # Сохраняем только сообщение об ошибке
+                    output_lines.append(message)
+
+                full_match = full_error.match(line)
+                if full_match:
+                    full_errors.append(line.strip())
+                    for next_line in file:
+                        if next_line.startswith('2025-'):
+                            break
+                        full_errors.append(next_line.strip())
+
             elif 'WARN' in line:
                 warns += 1
                 match = warn_pattern.search(line)
                 if match:
-                    message = match.group(1).strip()  # Извлекаем сообщение о предупреждении
+                    message = match.group(1).strip()
                     warn_messages[message] += 1
-                    output_lines.append(message)  # Сохраняем только сообщение о предупреждении
+                    output_lines.append(message)
 
-    with open(output_file, 'w') as of:
+                full_match = full_error.match(line)
+                if full_match:
+                    full_errors.append(line.strip())
+                    for next_line in file:
+                        if next_line.startswith('2025-'):
+                            break
+                        full_errors.append(next_line.strip())
+
+    with open(output_file, 'w', encoding='utf-8') as of:
         of.write(
-            f"\nФайл {input_file}.\nКоличество ошибок ERROR: {errors}, предупреждений WARN: {warns}.\nОбработано: {datetime.now()}\n\n")
+            f"\nФайл {input_file}.\nКоличество ошибок ERROR: {errors}, предупреждений WARN: {warns}.\nВремя обработки: {datetime.now()}\n\n")
 
         of.write("ERRORS:\n")
         for message, count in error_messages.items():
@@ -40,8 +59,11 @@ def parse_logs(input_file, output_file):
         for message, count in warn_messages.items():
             of.write(f"{message}: {count}\n")
 
+        of.write("\nПолные ошибки:\n")
+        for i in full_errors:
+            of.write(f"{i}\n")
 
-input_log_file = 'files/common.2025-03-19.0.log'
+input_log_file = 'files/testcommon.log'
 output_log_file = 'files/output_logs.txt'
 
 parse_logs(input_log_file, output_log_file)
